9dba8b7a7c06cbf21b1bdaa75992aa310d6f4d2f
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-20 12:53:31 +0200

    initial Commit

---
+++ src/main/java/org/codewarior/ridemyway/RidemywayApplication.java
@@ -0,0 +1,13 @@
+package org.codewarior.ridemyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidemywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidemywayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codewarior/ridemyway/RidemywayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codewarior.ridemyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidemywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
